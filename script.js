6eaa7c9ed24f836cee2e06cf28c7d66ee66be4b6
Nick LM <90866238+NickLM1200@users.noreply.github.com>
2024-12-02 15:15:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-    title: "Welcome to CS3210 Template Engine Lab",
-    content: "This is a simple template engine demonstration.",
-    author: "Dr.Red",
-};
-function renderTemplate(template, data) {
-    for (const key in data) {
-        const regex = new RegExp('{{' + key + '}}', 'g');
-        template = template.replace(regex, data[key]);
-    }
-    return template;
-}
-const template = `
-    <h3>{{title}}</h3>
-    <p>{{content}}</p>
-    <p>Author: {{author}}</p>
-`;
-const app = document.getElementById('app');
-app.innerHTML = renderTemplate(template, data);
 // Define a template using Mustache.js
 var template = `
     <ul>
